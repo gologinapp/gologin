e9509555cfeeec288cc8dd712f6981dc5bfc0a0c
Mixolap <mixolap@gmail.com>
2020-08-13 21:36:38 +0300

    walmart login

--- examples/example-login-walmart.js
+++ examples/example-login-walmart.js
@@ -5,8 +5,8 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 (async () =>{
     const GL = new GoLogin({
-        profile_id: '5eef762508f527445e8f0f5f',
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZTFjOWIwZTc5MmEyMDQ5MjhhZDU3ZDAiLCJ0eXBlIjoiZGV2Iiwiand0aWQiOiI1ZTkwZDViYTZiNmE5YTRjZGI2ODc1YTgifQ.jbQ-iuojv6Vijt82i7LrOBy46b4Kpxir5-E9sXntrqY',
+        profile_id: 'yU0token',
+        token: 'yU0Pr0f1leiD',
         executablePath: '/usr/bin/orbita-browser/chrome',
     });
 
@@ -20,8 +20,8 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
     await page.goto('https://www.walmart.com/account/profile'); 
     if( await page.evaluate((e)=>document.querySelector('#email')) ){
         // need login
-        await page.type('#email', 'mixolap@gmail.com');
-        await page.type('#password', '1q2w3e');
+        await page.type('#email', 'myemail');
+        await page.type('#password', 'mypassword');
         await page.click('[type=submit]');
     } 
     
