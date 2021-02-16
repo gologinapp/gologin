ca5d4af21156b1bcb2f52a78701e068e35058fdc
mixolap <mixolapus@yandex.ru>
2021-02-16 20:20:07 +0300

    example_amazon_viewport

--- examples/example-amazon.js
+++ examples/example-amazon.js
@@ -7,14 +7,23 @@ const GoLogin = require('../gologin');
         profile_id: 'yU0Pr0f1leiD',
         executablePath: '/usr/bin/orbita-browser/chrome',
     });
-    const wsUrl = await GL.start(); 
+    const {status, wsUrl} = await GL.start(); 
     const browser = await puppeteer.connect({
         browserWSEndpoint: wsUrl.toString(), 
         ignoreHTTPSErrors: true,
     });
 
     const page = await browser.newPage();
+
+    const viewPort = GL.getViewPort();
+    await page.setViewport({ width: Math.round(viewPort.width * 0.994), height: Math.round(viewPort.height * 0.92) });
+    const session = await page.target().createCDPSession();
+    const { windowId } = await session.send('Browser.getWindowForTarget');
+    await session.send('Browser.setWindowBounds', { windowId, bounds: viewPort });
+    await session.detach();
+    
     await page.goto('https://www.amazon.com/-/dp/B07GRM747Y');   
+
     const content = await page.content();
     const matchData = content.match(/'initial': (.*)}/);
     if(matchData == null || matchData.length==0){
