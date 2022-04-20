19cd66211beffdf77ae12f3dcd54d5b9d38811ac
Alexandivv <98816693+alexandivv@users.noreply.github.com>
2022-04-20 13:12:53 +0300

    add example startRemote

---
+++ examples/example-startremote.js
@@ -0,0 +1,26 @@
+// Usage example: in the terminal enter
+// node example-startremote.js yU0token yU0Pr0f1leiD
+
+// your token api (located in the settings, api) 
+// https://github.com/gologinapp/gologin#usage
+let GOLOGIN_API_TOKEN = process.argv[2];
+// your profile id
+let GOLOGIN_PROFILE_ID = process.argv[3];
+
+const GoLogin = require('../gologin');
+
+
+(async () =>{
+    const GL = new GoLogin({  
+        token: GOLOGIN_API_TOKEN,
+        profile_id: GOLOGIN_PROFILE_ID,
+    });
+    // connection of the remote work method
+    const {status, wsUrl} = await GL.startRemote();
+
+
+    GOLOGIN_PROFILE_CLOUD_URL = wsUrl.split('/')[2]
+    console.log('Done! Launch web browser and navigate to URL:', GOLOGIN_PROFILE_CLOUD_URL);
+})();
+
+// after running the script, the url will appear on the terminal
