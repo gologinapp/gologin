e2af0a7107b17e5ac6dca1d8a5deaa58565216ee
Dmitry Kaigorodov <dima.kaigorodov@gmail.com>
2024-07-08 07:59:43 +0200

    add semicolumns

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,32 @@
-import { type Browser } from "puppeteer-core/lib/Browser";
+import { type Browser } from 'puppeteer-core/lib/Browser';
 
 export const OSES = {
   win: 'win',
   lin: 'lin',
   mac: 'mac',
-  android: 'android'
+  android: 'android',
 } as const;
 export type OsType = (typeof OSES)[keyof typeof OSES];
 
-
 type CloudLaunchParams = {
-  cloud: true,
+  cloud: true;
 
   /**
    * Format: 'dataCenter:DE'
    */
-  geolocation?: string
-}
+  geolocation?: string;
+};
 type LocalLaunchParams = {
-  cloud: false,
-  headless: boolean,
-}
+  cloud: false;
+  headless: boolean;
+};
 
 type ExistingProfileLaunchParams = {
-  profile_id: string
-}
+  profile_id: string;
+};
 type NewProfileLaunchParams = {
-  proxyGeolocation: string
-}
+  proxyGeolocation: string;
+};
 
 type LaunchParams =
   | CloudLaunchParams
@@ -35,28 +34,27 @@ type LaunchParams =
   | ExistingProfileLaunchParams
   | NewProfileLaunchParams
   | {
-    /**
-     * default delay, 250 
-     */
-    defaultDelay: number,
+      /**
+       * default delay, 250
+       */
+      defaultDelay: number;
 
-    /**
-     * Operating system
-     */
-    os: OsType
-  }
+      /**
+       * Operating system
+       */
+      os: OsType;
+    };
 
 type LaunchFn = (params?: LaunchParams) => Promise<Browser>;
 
 type GologinApiType = {
-  launch: LaunchFn,
-  exit: (status = 0) => Promise<void>
-  delay: (ms: number) => Promise<void>
+  launch: LaunchFn;
+  exit: (status = 0) => Promise<void>;
+  delay: (ms: number) => Promise<void>;
 };
 
 type GologinApiParams = {
-  token: string
-}
+  token: string;
+};
 
 export declare function GologinApi(params: GologinApiParams): GologinApiType;
-
--- src/gologin-api.js
+++ src/gologin-api.js
@@ -1,60 +1,61 @@
-
 import puppeteer from 'puppeteer-core';
-import GoLogin from "./gologin.js";
-
+import GoLogin from './gologin.js';
 
 export function getDefaultParams() {
   return {
     token: process.env.GL_API_TOKEN,
     profile_id: process.env.GL_PROFILE_ID,
     executablePath: process.env.GL_EXECUTABLE_PATH,
-  }
+  };
 }
 
 const createLegacyGologin = (params) => {
-  const defaults = getDefaultParams()
+  const defaults = getDefaultParams();
   return new GoLogin({
     ...defaults,
     ...params,
   });
-}
+};
 
-export const delay = (ms = 250) => new Promise(res => setTimeout(res, ms));
+export const delay = (ms = 250) => new Promise((res) => setTimeout(res, ms));
 
 export function GologinApi({ token }) {
   if (!token) {
-    throw new Error("GoLogin API token is missing")
+    throw new Error('GoLogin API token is missing');
   }
   let browsers = [];
   let legacyGls = [];
 
   const launchLocal = async (params) => {
-    console.log("launchExistingProfile", params)
-    const legacyGologin = createLegacyGologin(params)
-    const started = await legacyGologin.startLocal()
-    console.debug({ started })
+    console.log('launchExistingProfile', params);
+    const legacyGologin = createLegacyGologin(params);
+    const started = await legacyGologin.startLocal();
+    console.debug({ started });
     const browser = await puppeteer.connect({
       browserWSEndpoint: started.wsUrl,
       ignoreHTTPSErrors: true,
     });
-    browsers.push(browser)
+    browsers.push(browser);
     return browser;
-  }
+  };
 
   const launchCloudProfile = async (params) => {
-    console.log("launchCloudProfile", params)
-    const profileParam = params.profile_id ? `&profile=${params.profile_id}` : ''
-    const geolocationParam = params.geolocation ? `&geolocation=${params.geolocation}` : ''
-    const browserWSEndpoint =
-      `https://cloud.gologin.com/connect?token=${token}${profileParam}${geolocationParam}`
-    console.debug(browserWSEndpoint)
+    console.log('launchCloudProfile', params);
+    const profileParam = params.profile_id
+      ? `&profile=${params.profile_id}`
+      : '';
+    const geolocationParam = params.geolocation
+      ? `&geolocation=${params.geolocation}`
+      : '';
+    const browserWSEndpoint = `https://cloud.gologin.com/connect?token=${token}${profileParam}${geolocationParam}`;
+    console.debug(browserWSEndpoint);
     const browser = await puppeteer.connect({
       browserWSEndpoint,
       ignoreHTTPSErrors: true,
     });
-    browsers.push(browser)
+    browsers.push(browser);
     return browser;
-  }
+  };
 
   return {
     /**
@@ -64,31 +65,33 @@ export function GologinApi({ token }) {
      * @returns browser
      */
     async launch(params = {}) {
-      console.log("launch", params)
+      console.log('launch', params);
       if (params.cloud) {
-        return await launchCloudProfile(params)
+        return await launchCloudProfile(params);
       }
       if (params.profile_id) {
-        return await launchLocal(params)
+        return await launchLocal(params);
       }
       if (params.geolocation) {
-        return await launchLocal(params)
+        return await launchLocal(params);
       }
     },
 
     async page(url, params) {
-      let browser = await this.launch()
-      const page = await browser.newPage()
-      await page.goto(url, { waitUntil: 'networkidle2' })
-      return { page, browser, session }
+      let browser = await this.launch();
+      const page = await browser.newPage();
+      await page.goto(url, { waitUntil: 'networkidle2' });
+      return { page, browser, session };
     },
 
     async exit(status = 0) {
-      Promise.allSettled(browsers.map(browser => browser.close()))
-      Promise.allSettled(legacyGls.map(gl => gl.stopLocal({ posting: false })))
-      process.exit(status)
+      Promise.allSettled(browsers.map((browser) => browser.close()));
+      Promise.allSettled(
+        legacyGls.map((gl) => gl.stopLocal({ posting: false })),
+      );
+      process.exit(status);
     },
 
-    delay
-  }
+    delay,
+  };
 }
