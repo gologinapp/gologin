b3dae78be665850d561c7d6bdccd803950757b0c
igortikushEasync <igor@easync.io>
2025-02-04 16:55:36 +0100

    cookies windows fix

--- example.js
+++ example.js
@@ -1,36 +1,82 @@
-import puppeteer from 'puppeteer-core';
+// import { spawn } from 'child_process';
+import os from 'os';
 
+// import { connect } from 'puppeteer-core';
 import GoLogin from './src/gologin.js';
 
-const token = 'yU0token';
-const profile_id = 'yU0Pr0f1leiD';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MzczNmY5N2YxZjBhMjBjNjI5NTVlYzIiLCJ0eXBlIjoiZGV2Iiwiand0aWQiOiI2NzkwYzQ1ZTBjNTA1NzA0MmJlYTE1ZDAifQ.YcTJy--42-FjeuWdx5tSlPiGq7LP_QPXowOmm2WVT7g';
+const profile_id = '67a1f8fa6b01ccb71bd14563';
 
 (async () => {
   const GL = new GoLogin({
     token,
-    profile_id,
   });
 
-  const { status, wsUrl } = await GL.start().catch((e) => {
-    console.trace(e);
+  // const profile_id = await GL.create({
+  //   name: 'profile_gmail',
+  //   os: 'win',
+  //   navigator: {
+  //     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+  //     resolution: '1280x720',
+  //     language: 'en-GB,en-US;q=0.9,en;q=0.8',
+  //     hardwareConcurrency: 8,
+  //     deviceMemory: 8,
+  //     maxTouchPoints: 5,
+  //   },
+  //   proxy: {
+  //     mode: 'http',
+  //     host: '',
+  //     port: '',
+  //     username: '',
+  //     password: '',
+  //   },
+  // });
 
-    return { status: 'failure' };
-  });
+  console.log('profile id=', profile_id);
+  GL.setProfileId(profile_id);
 
-  if (status !== 'success') {
-    console.log('Invalid status');
+  const { status, wsUrl } = await GL.start();
+  // const browser = await connect({
+  //   browserWSEndpoint: wsUrl.toString(),
+  //   ignoreHTTPSErrors: true,
+  // });
 
-    return;
-  }
+  // if (status !== 'success') {
+  //   console.error('Failed to start GoLogin profile');
 
-  const browser = await puppeteer.connect({
-    browserWSEndpoint: wsUrl.toString(),
-    ignoreHTTPSErrors: true,
-  });
+  //   return;
+  // }
 
-  const page = await browser.newPage();
-  await page.goto('https://myip.link/mini');
-  console.log(await page.content());
-  await browser.close();
+  // const page = await browser.newPage();
+
+  // Step 1: Log in to the site
+  // const LOGIN_URL = 'https://the-internet.herokuapp.com/login';
+  // const USERNAME = 'tomsmith'; // Provided by the test site
+  // const PASSWORD = 'SuperSecretPassword!'; // Provided by the test site
+
+  // await page.goto(LOGIN_URL, { waitUntil: 'networkidle2' });
+
+  // // Automate filling in the username and password fields
+  // await page.type('#username', USERNAME);
+  // await page.type('#password', PASSWORD);
+
+  // // Submit the login form
+  // await page.click('button[type="submit"]');
+  // await new Promise(r => setTimeout(r, 2000));
+  // // Confirm login success by checking for a specific element or text
+  // const loginSuccess = await page.evaluate(() =>
+  //   document.body.textContent.includes('You logged into a secure area!'),
+  // );
+
+  // console.log('Login successful:', loginSuccess);
+
+  // Save cookies after login
+  // const cookies = await page.cookies();
+  // console.log('Cookies after login:', cookies);
+  await new Promise(r => setTimeout(r, 120000));
+
+  // await browser.close();
   await GL.stop();
 })();
+
+console.log(os.version());
--- examples/example-amazon-cloud-browser.js
+++ examples/example-amazon-cloud-browser.js
@@ -4,21 +4,17 @@
 // your token api (located in the settings, api)
 // https://github.com/gologinapp/gologin#usage
 
-import GoLogin from 'gologin';
 import puppeteer from 'puppeteer-core';
 
-const [_execPath, _filePath, GOLOGIN_API_TOKEN, GOLOGIN_PROFILE_ID] = process.argv;
+const [_execPath, _filePath] = process.argv;
+const GOLOGIN_API_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MzczNmY5N2YxZjBhMjBjNjI5NTVlYzIiLCJ0eXBlIjoiZGV2Iiwiand0aWQiOiI2NzkwYzQ1ZTBjNTA1NzA0MmJlYTE1ZDAifQ.YcTJy--42-FjeuWdx5tSlPiGq7LP_QPXowOmm2WVT7g';
+const GOLOGIN_PROFILE_ID = '67a12a9fb20c1fdd8921a165';
 
 (async () => {
-  const GL = new GoLogin({
-    token: GOLOGIN_API_TOKEN,
-    profile_id: GOLOGIN_PROFILE_ID,
-  });
-
   const browser = await puppeteer.connect({
     browserWSEndpoint: `https://cloudbrowser.gologin.com/connect?token=${GOLOGIN_API_TOKEN}&profile=${GOLOGIN_PROFILE_ID}`,
     ignoreHTTPSErrors: true,
-  });
+  }).catch(console.log);
 
   const page = await browser.newPage();
 
@@ -33,5 +29,14 @@ const [_execPath, _filePath, GOLOGIN_API_TOKEN, GOLOGIN_PROFILE_ID] = process.ar
     console.log('images=', images);
   }
 
-  await GL.stopRemote();
+  await new Promise(resolve => setTimeout(resolve, 120000));
+  console.log('stopping browser');
+  await fetch(`https://api.gologin.com/browser/${GOLOGIN_PROFILE_ID}/web`, {
+    headers: {
+      'Authorization': `Bearer ${GOLOGIN_API_TOKEN}`,
+    },
+    method: 'DELETE',
+  });
+
+  console.log('browser stopped');
 })();
