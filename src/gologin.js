1869d57f7c8ab603350f783e34aae07a02462878
igortikushEasync <91466329+igortikushEasync@users.noreply.github.com>
2025-05-29 18:23:35 +0200

    Update gologin.js

--- src/gologin.js
+++ src/gologin.js
@@ -937,7 +937,7 @@ export class GoLogin {
 
     if (this.waitWebsocket) {
       debug('GETTING WS URL FROM BROWSER');
-      const data = await requests.get(`http://127.0.0.1:${remote_debugging_port}/json/version`, { json: true, maxAttempts: 10, retryDelay: 1000 });
+      const data = await requests.get(`http://127.0.0.1:${remote_debugging_port}/json/version`, { json: true, maxAttempts: 30, retryDelay: 1000 });
 
       debug('WS IS', get(data, 'body.webSocketDebuggerUrl', ''));
       this.is_active = true;
